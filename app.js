3b159d6d7d828bb0ed33c8f845bf04adfba03acb
rohanbhore <rohan@gmail.com>
2017-02-03 16:55:22 +0530

    first commit

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ function ShoppingListShowController(ShoppingListService)
   {
     var showList=this;
     showList.items=ShoppingListService.getItems();
+    showList.removeItem=function(itemIndex)
+    {
+      ShoppingListService.removeItem(itemIndex);
+    };
   }
 
 function ShoppingListService()
@@ -38,6 +42,10 @@ function ShoppingListService()
                   quantity : quantity
                 };
                items.push(item);//check
+    };
+    service.removeItem=function(itemIndex)
+    {
+      items.splice(itemIndex,1);
     };
     service.getItems=function()
     {
